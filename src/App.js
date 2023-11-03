11067592f69b685ade7ea46d8b31c9e3285e9726
magdiii <mohamed.magdi.deif@gmail.com>
2023-11-03 23:32:41 +0200

    frat: implements app router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Courses from "./Pages/Courses/Courses";
+import Contact from "./Pages/Contact/Contact";
+import Header from "./Components/Header/Header";
+import { Provider } from "react-redux";
+import store from "./Store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/course" element={<Courses />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
